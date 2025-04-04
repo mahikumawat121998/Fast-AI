f293273d13b28a7dfef90ef4fa43f32ee94b1cbe
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-04 16:52:13 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-  origin: ['http://localhost:5173', 'http://192.168.1.11:5173',],
+  origin: ['http://localhost:5173', 'http://192.168.1.11:5173','http://16.176.26.164/:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true // Enable credentials if needed
   })
