38a8e166b8793060bc0fbe1df4396955652043c6
MAHESH KUMAWAT <mahikumawat121998@gmail.com>
2025-04-04 16:19:42 +0530

    Updating vite config file

--- CLIENT/vite.config.js
+++ CLIENT/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: 5173,      // You can keep this or update it if necessary
+    open: true,      // Automatically open in the browser when started
+  },
 })
